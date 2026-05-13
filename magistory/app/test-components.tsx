7ab8c5311eec82d02e1fee45e5e302c5a9d1e367
Filla Ramadhan <fillar71@gmail.com>
2026-05-14 01:46:08 +0700

    fix: update test-components.tsx with correct import paths

--- magistory/app/test-components.tsx
+++ magistory/app/test-components.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardHeader,
@@ -6,7 +6,7 @@ import {
   CardDescription,
   CardContent,
   CardFooter
-} from "../components/ui/card"
+} from "@/components/ui/card"
 
 export default function TestComponents() {
   return (
